5ca67c52fe9a0ae998dcbce20fcad2270544a28d
coni <nicolas.jove.cubillo@gregoriofer.com>
2025-09-22 09:27:03 +0200

    Aplicamos ejecución concurrente haciendo que la clase hijo se junte con la del padre

--- Ejem01_HolaMundoThread/src/Aplicacion.java
+++ Ejem01_HolaMundoThread/src/Aplicacion.java
@@ -3,16 +3,16 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hola mundo");
-		
+		System.out.println("Hola mundo");
+
 		Proceso p = new Proceso();
-		
+		p.start();
+
 		for (int i = 0; i < 11; i++) {
 			System.out.println("Soy Proceso PADRE: " + i);
-			
-			
+
 		}
-		
+
 	}
 
 }
