a6c4d1aabb995df0a41388acbac99de97754f493
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2025-10-20 08:45:58 +0200

    Add synchronization to counter update in Proceso
    Wrapped the counter increment and print statements in a synchronized block to ensure thread safety. Added a short sleep to simulate execution delay and encourage context switching between threads.

--- Ejem04_CompartirEsVivir/src/Proceso.java
+++ Ejem04_CompartirEsVivir/src/Proceso.java
@@ -15,10 +15,25 @@ public class Proceso extends Thread {
 	public void run() {
 		// TODO Auto-generated method stub
 		for ( int i = 0; i < 100; i++) {
-			int temp = contador.getValor();
-			temp++;
-			contador.setValor(temp);
-			System.out.println(this.getName()+"Valor final del contador: " + contador.getValor());
+			synchronized (contador) { // Fona de sincronización donde solo puede acceder un hilo a la vez
+									 // asta que el hilo salga del bloque synchronized no puede entrar 
+									// otro hilo a este bloque. 
+								   // Esta zona debe ser lo mas pequeña posible para evitar cuellos 
+								  //de botella.
+				int temp = contador.getValor();
+				temp++;
+				contador.setValor(temp);
+				System.out.println(this.getName()+"Valor final del contador: " + contador.getValor());
+			}
+			
+			//sleep para simular que tarda un poco en ejecutar para forzar el cambio de contexto entre hilos
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 	}
 }
